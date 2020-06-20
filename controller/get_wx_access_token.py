9d6ec8a5519927476d5010b583ea1a9d97ae2500
houxiwu <xifive@163.com>
2020-06-20 14:57:07 +0800

    Update get_wx_access_token.py

--- controller/get_wx_access_token.py
+++ controller/get_wx_access_token.py
@@ -1,4 +1,4 @@
-import requests
+'''import requests
 
 
 appid = wxf64d39f0756b3f70
@@ -6,4 +6,9 @@ secret = da5da13fd71812c61cb08bc1e8a569b0
 
 def get_access_token():
     access_token = requests.get(https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET)
-    return access_token
+    return access_token'''
+
+
+import werobot.utils
+
+print(werobot.utils.generate_token())
